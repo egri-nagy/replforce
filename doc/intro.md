86e0932536d1206e3440b6ee5a731fffc021c0a1
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-07-08 08:45:37 +1000

    using clojure 1.7.0

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to replforce
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/replforce/core_test.clj
@@ -0,0 +1,7 @@
+(ns replforce.core-test
+  (:require [clojure.test :refer :all]
+            [replforce.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
